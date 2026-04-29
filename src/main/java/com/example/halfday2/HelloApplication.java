38241c4062d4171cfe7bbb539c717ee80ea0c9d0
sarah <student.189241@worcesterschools.net>
2026-04-29 12:24:53 -0400

    no apinowd

--- src/main/java/com/example/halfday2/HelloApplication.java
+++ src/main/java/com/example/halfday2/HelloApplication.java
@@ -3,10 +3,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
-import java.util.Scanner;
-
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
