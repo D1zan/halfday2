1a5416eaa3b7ad515522968e22a621be7e3ac309
sarah <student.189241@worcesterschools.net>
2026-04-29 11:32:35 -0400

    push

---
+++ src/main/java/com/example/halfday2/Main.java
@@ -0,0 +1,15 @@
+package com.example.halfday2;
+
+import java.util.Scanner;
+
+import static jdk.internal.agent.Agent.getText;
+
+public class Main {
+    Scanner scan = new Scanner(System.in);
+    boolean web = true;
+
+    while(web) {
+        String city = getText(city_name);
+
+    }
+}
---
+++ src/main/java/com/example/halfday2/Weather.java
@@ -0,0 +1,20 @@
+package com.example.halfday2;
+
+public class Weather {
+    private int temperature;
+
+    private String city;
+
+    public Weather( int temperature, String city) {
+        this.temperature = temperature;
+        this.city = city;
+    }
+
+    private int getTemperature() {
+        return temperature;
+    }
+
+    private String getCity() {
+        return city;
+    }
+}
