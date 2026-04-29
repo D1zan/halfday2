46b9b26e582a3a29191289d2700ed25cc02b9ccb
sarah <student.189241@worcesterschools.net>
2026-04-29 12:09:26 -0400

    no apinow

--- src/main/java/com/example/halfday2/API.java
+++ src/main/java/com/example/halfday2/API.java
@@ -12,11 +12,11 @@ import java.net.http.HttpResponse;
 public class API {
     public static Weather getWeather(String city, int temperature)
             throws IOException, InterruptedIOException, InterruptedException {
-            String url = "https://api.openweathermap.org/data/2.5/weather?q=&units=metric&appid=6b6fd6ef03e57c965e9ed142598b7a14";
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder() .uri(URI.create(url)) .build();
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
+        String url = "https://api.openweathermap.org/data/2.5/weather?q=&units=metric&appid=6b6fd6ef03e57c965e9ed142598b7a14";
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        return new Weather (temperature, city);
     }
 
 }
